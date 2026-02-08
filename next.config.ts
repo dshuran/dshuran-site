976a524f09081e433223c6b165b407da5ba31472
Dmitry Shuran <dmitry.shuran@gmail.com>
2026-02-08 19:23:33 +0300

    поправил загрузку картинок

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export'
-  /* config options here */
+  output: 'export',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
